9f721359ac3f81497d243e56492701f3c5f6e3b8
yhfwork <mm20102010mm>
2022-11-08 02:04:48 +0800

    update

--- lib/f_yc_entity.dart
+++ lib/f_yc_entity.dart
@@ -4,3 +4,7 @@ export './src/f_yc_entitys_behavior.dart';
 export './src/f_yc_entitys_user.dart';
 export './src/f_yc_entitys_wallet.dart';
 export './src/f_yc_entitys_remote_config.dart';
+export './src/f_yc_entitys_events_behavior_update.dart';
+export './src/f_yc_entitys_events_user_info_update.dart';
+export './src/f_yc_entitys_events_wallet_update.dart';
+export './src/f_yc_entitys_events_welfare_re_update.dart';
